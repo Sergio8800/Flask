1f90011c13e5edfdfa1129c85765a08d912379f5
Sergio8800 <wasp8800@gmail.com>
2024-04-27 13:43:27 +0200

    finish22

--- task_05_finish/app_fastapi.py
+++ task_05_finish/app_fastapi.py
@@ -9,28 +9,58 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from models import *
 
-DATABASE_URL = "sqlite:///./instance/events.db"
+DATABASE_URL = "sqlite:///instance/events_api.db"
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
+users = sqlalchemy.Table("users", metadata, sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+                         sqlalchemy.Column("name", sqlalchemy.String(32)),
+                         sqlalchemy.Column("email", sqlalchemy.String(128)), )
+goods = sqlalchemy.Table("goods", metadata, sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+                         sqlalchemy.Column("title", sqlalchemy.String(32)),
+                         sqlalchemy.Column("content", sqlalchemy.String(32)),
+                         sqlalchemy.Column("price", sqlalchemy.Float(12)), )
 
-# engine = sqlalchemy.create_engine(DATABASE_URL)
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
 metadata.create_all(engine)
 app = FastAPI()
 
 
-# class User(BaseModel):
-#     id: int
-#     name: str = Field(max_length=32)
-#     email: str = Field(max_length=128)
-#     password: int = Field(max_length=128)
+class User(BaseModel):
+    id: int
+    name: str = Field(max_length=32)
+    email: str = Field(max_length=128)
+    # password: int = Field(default=123, title="Password")
+
+
+class UserIn(BaseModel):
+    name: str = Field(max_length=32)
+    email: str = Field(max_length=128)
+    # password: int = Field(default=123, title="Password")
+
+
+class Goods(BaseModel):
+    id: int
+    title: str = Field(max_length=30)
+    content: str
+    price: float = Field(default=110)
+    # img: list = []
+
+
+class Goods1(BaseModel):
+    title: str = Field(max_length=30)
+    content: str
+    price: float = Field(default=110)
+
+
+class Order(BaseModel):
+    goods: List[Goods]
+    user: User
 
 
 @app.get("/users/", response_model=List[User])
 async def read_users():
-    query = User.select()
+    query = users.select()
     print(database)
     print(DATABASE_URL)
     return await database.fetch_all(query)
@@ -38,27 +68,27 @@ async def read_users():
 
 @app.get("/users/{user_id}", response_model=User)
 async def read_user(user_id: int):
-    query = User.select().where(User.c.id == user_id)
+    query = users.select().where(User.c.id == user_id)
     return await database.fetch_one(query)
 
 
 @app.post("/users/", response_model=User)
-async def create_user(user: User):
-    query = User.insert().values(name=user.name, email=user.email)
+async def create_user(user: UserIn):
+    query = users.insert().values(name=user.name, email=user.email)
     last_record_id = await database.execute(query)
     return {**user.dict(), "id": last_record_id}
 
 
 @app.put("/users/{user_id}", response_model=User)
-async def update_user(user_id: int, new_user: User):
-    query = User.update().where(User.c.id == user_id).values(**new_user.dict())
+async def update_user(user_id: int, new_user: UserIn):
+    query = users.update().where(User.c.id == user_id).values(**new_user.dict())
     await database.execute(query)
     return {**new_user.dict(), "id": user_id}
 
 
 @app.delete("/users/{user_id}")
 async def delete_user(user_id: int):
-    query = User.delete().where(User.c.id == user_id)
+    query = users.delete().where(User.c.id == user_id)
     await database.execute(query)
     return {'message': 'User deleted'}
 
@@ -75,4 +105,3 @@ async def shutdown():
 
 if __name__ == '__main__':
     uvicorn.run("app_fastapi:app", port=8008)
-
